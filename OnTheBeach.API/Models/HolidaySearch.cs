27bc8f003a64a68110eff7af0b85c0d5dbddc1b4
Samuel Bradley <samuel@samuel-bradley.com>
2025-01-25 00:44:06 +0000

    Add HolidaySearch model

---
+++ OnTheBeach.API/Models/HolidaySearch.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+using NodaTime;
+
+namespace OnTheBeach.Models
+{
+    public class HolidaySearch
+    {
+        [JsonPropertyName("departing_from")]
+        public string DepartingFrom { get; }
+
+        [JsonPropertyName("traveling_to")]
+        public string TravelingTo { get; }
+
+        [JsonPropertyName("departure_date")]
+        public LocalDate DepartureDate { get; }
+
+        [JsonPropertyName("duration")]
+        public int Duration { get; }
+
+        [JsonConstructor]
+        public HolidaySearch(string departingFrom, string travelingTo, LocalDate departureDate, int duration)
+        {
+            DepartingFrom = departingFrom;
+            TravelingTo = travelingTo;
+            DepartureDate = departureDate;
+            Duration = duration;
+        }
+    }
+}
