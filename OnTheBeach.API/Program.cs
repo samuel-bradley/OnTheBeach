9854218695ab21cea3919aad8a36736ca7363936
Samuel Bradley <samuel@samuel-bradley.com>
2025-01-26 12:53:51 +0000

    Load data into repositories

--- OnTheBeach.API/Program.cs
+++ OnTheBeach.API/Program.cs
@@ -1,15 +1,31 @@
+using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
+using OnTheBeach.Models;
+using OnTheBeach.Repositories;
+using System.Text.Json;
+
+// Create the builder
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Load flight and hotel data from JSON files
+var flightJson = File.ReadAllText("Data/flights.json");
+var hotelJson = File.ReadAllText("Data/hotels.json");
+var options = new JsonSerializerOptions().ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+List<Flight> flights = JsonSerializer.Deserialize<List<Flight>>(flightJson, options) ?? [];
+List<Hotel> hotels = JsonSerializer.Deserialize<List<Hotel>>(hotelJson, options) ?? [];
+
+// Register repositories with loaded data
+builder.Services.AddSingleton<FlightRepository>(new FlightRepository(flights));
+builder.Services.AddSingleton<HotelRepository>(new HotelRepository(hotels));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +33,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
