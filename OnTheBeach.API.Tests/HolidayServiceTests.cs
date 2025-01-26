38a751ade0019687ca1aa8d42ed99f9374af79b3
Samuel Bradley <samuel@samuel-bradley.com>
2025-01-26 18:20:49 +0000

    Make service test more robust by adding a hotel and flight that won't match on duration/date

--- OnTheBeach.API.Tests/HolidayServiceTests.cs
+++ OnTheBeach.API.Tests/HolidayServiceTests.cs
@@ -15,12 +15,15 @@ namespace OnTheBeach.API.Tests
         {
             List<Flight> flights = [
                 new Flight(1, "Airline1", "London", "Madrid", 200, new LocalDate(2025, 6, 15)),
-                new Flight(2, "Airline2", "London", "Madrid", 250, new LocalDate(2025, 6, 15))
+                new Flight(2, "Airline2", "London", "Madrid", 250, new LocalDate(2025, 6, 15)),
+                new Flight(3, "Airline3", "London", "Madrid", 250, new LocalDate(2027, 7, 17)),
+                new Flight(4, "Airline4", "Paris", "Madrid", 250, new LocalDate(2025, 6, 15))
             ];
             List<Hotel> hotels = [
                 new Hotel(1, "Hotel1", new LocalDate(2025, 6, 15), 100, ["Madrid"], 6),
                 new Hotel(2, "Hotel2", new LocalDate(2025, 6, 15), 120, ["Madrid"], 6),
-                new Hotel(3, "Hotel3", new LocalDate(2025, 6, 15), 150, ["Barcelona"], 6)
+                new Hotel(3, "Hotel3", new LocalDate(2025, 6, 15), 150, ["Barcelona"], 6),
+                new Hotel(4, "Hotel4", new LocalDate(2025, 6, 15), 120, ["Madrid"], 12),
             ];
             _flightRepository = new FlightRepository(flights);
             _hotelRepository = new HotelRepository(hotels);
@@ -39,6 +42,7 @@ namespace OnTheBeach.API.Tests
             Assert.Contains(results, h => h.Flight.Airline == "Airline1" && h.Hotel.Name == "Hotel2");
             Assert.Contains(results, h => h.Flight.Airline == "Airline2" && h.Hotel.Name == "Hotel1");
             Assert.Contains(results, h => h.Flight.Airline == "Airline2" && h.Hotel.Name == "Hotel2");
+            // Doesn't include flight from Paris, flight with wrong date, hotel in Barcelona or hotel for 12 nights
         }
 
         [Fact]
@@ -49,10 +53,10 @@ namespace OnTheBeach.API.Tests
             var results = _holidayService.FindHolidays(search);
 
             Assert.Equal(4, results.Count);
-            Assert.Equal(800, results[0].Price());
-            Assert.Equal(850, results[1].Price());
-            Assert.Equal(920, results[2].Price());
-            Assert.Equal(970, results[3].Price());
+            Assert.Equal(800, results[0].Price()); // 200 flight + 6 * 100 hotel
+            Assert.Equal(850, results[1].Price()); // 250 flight + 6 * 100 hotel
+            Assert.Equal(920, results[2].Price()); // 200 flight + 6 * 120 hotel
+            Assert.Equal(970, results[3].Price()); // 250 flight + 6 * 120 hotel
         }
 
         [Fact]
