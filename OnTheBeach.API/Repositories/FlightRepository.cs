358f572c4c1878a7be747c773a8dd853a0df15f4
Samuel Bradley <samuel@samuel-bradley.com>
2025-01-26 12:44:53 +0000

    Add repositories for flights and hotels

---
+++ OnTheBeach.API/Repositories/FlightRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnTheBeach.Models;
+
+namespace OnTheBeach.Repositories
+{
+    public class FlightRepository
+    {
+        private readonly List<Flight> _flights;
+
+        public FlightRepository(List<Flight> flights)
+        {
+            _flights = flights ?? throw new ArgumentNullException(nameof(flights));
+        }
+
+        public List<Flight> GetAllFlights()
+        {
+            return _flights;
+        }
+    }
+}
---
+++ OnTheBeach.API/Repositories/HotelRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnTheBeach.Models;
+
+namespace OnTheBeach.Repositories
+{
+    public class HotelRepository
+    {
+        private readonly List<Hotel> _hotels;
+
+        public HotelRepository(List<Hotel> hotels)
+        {
+            _hotels = hotels ?? throw new ArgumentNullException(nameof(hotels));
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            return _hotels;
+        }
+    }
+}
